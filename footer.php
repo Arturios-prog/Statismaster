96e2263599a726e58fd512eaa1d2cda23c30dbf7
Artem <fedodeev1artem@gmail.com>
2020-12-20 00:52:16 +0300

    commit 6

--- footer.php
+++ footer.php
@@ -18,7 +18,7 @@
 
       <!--Grid column-->
       <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 class="text-uppercase">Мои соцсети</h5>
+        <h5 class="text-uppercase">Контакты</h5>
 
         <ul class="list-unstyled mb-0">
           <li>
